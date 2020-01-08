fe12c4c6e42db38124077a486947dbba6018a3e2
saurabh13209 <agrawal99saurabh@gmail.com>
2020-01-08 17:46:41 +0530

    Blank project

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Alert, View, Text, } from 'react-native';
+import { Header, LearnMoreLinks, Colors, DebugInstructions, ReloadInstructions, } from 'react-native/Libraries/NewAppScreen';
+import firebase from 'react-native-firebase';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>
+          Kickme
+        </Text>
+      </View>
+    );
+  }
+}
